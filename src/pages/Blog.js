ef040a151be9d1fb9cb8240b05d25e6b6674e97e
om8912 <69741614+om8912@users.noreply.github.com>
2025-10-03 14:42:07 +0930

    Create Blog.js in src/pages

---
+++ src/pages/Blog.js
@@ -0,0 +1,13 @@
+function Blog() {
+  return (
+    <main>
+      <h1>Blog</h1>
+      <div>
+        <h2>How to Build a Brand Online</h2>
+        <p>Tips and strategies from our experts.</p>
+      </div>
+    </main>
+  );
+}
+
+export default Blog;

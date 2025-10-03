b5ff43f4f360ea20ac4ab680559ffcfdebbb9074
om8912 <69741614+om8912@users.noreply.github.com>
2025-10-03 14:41:34 +0930

    Create Services.js component

---
+++ src/pages/Services.js
@@ -0,0 +1,15 @@
+function Services() {
+  return (
+    <main>
+      <h1>Our Services</h1>
+      <ul>
+        <li>Web Design</li>
+        <li>Digital Marketing</li>
+        <li>Brand Strategy</li>
+        <li>UI/UX Consulting</li>
+      </ul>
+    </main>
+  );
+}
+
+export default Services;

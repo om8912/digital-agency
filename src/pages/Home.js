f36f608920cfa54f855fd71d334094c8a0d19f19
om8912 <69741614+om8912@users.noreply.github.com>
2025-10-03 14:41:25 +0930

    Create Home.js in src/pages

---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+function Home() {
+  return (
+    <main>
+      <section>
+        <h1>Welcome to Our Digital Agency</h1>
+        <p>We help businesses grow online with creative solutions.</p>
+      </section>
+    </main>
+  );
+}
+
+export default Home;

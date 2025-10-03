298376fd31ac291dc57279a82913681457c3a2bd
om8912 <69741614+om8912@users.noreply.github.com>
2025-10-03 14:41:50 +0930

    Create CaseStudy.js in src/pages

---
+++ src/pages/CaseStudy.js
@@ -0,0 +1,13 @@
+function CaseStudy() {
+  return (
+    <main>
+      <h1>Case Studies</h1>
+      <div>
+        <h2>Brand X Transformation</h2>
+        <p>Read how we helped Brand X grow 200% in one year.</p>
+      </div>
+    </main>
+  );
+}
+
+export default CaseStudy;

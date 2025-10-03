051464f3d56e7e0b4e5fac4f26f4f60a2c6e7f21
om8912 <69741614+om8912@users.noreply.github.com>
2025-10-03 14:42:27 +0930

    Create Gallery.js file in src/pages

---
+++ src/pages/Gallery.js
@@ -0,0 +1,13 @@
+function Gallery() {
+  return (
+    <main>
+      <h1>Gallery</h1>
+      <div className="gallery">
+        <img src="/assets/img1.jpg" alt="Work 1" />
+        <img src="/assets/img2.jpg" alt="Work 2" />
+      </div>
+    </main>
+  );
+}
+
+export default Gallery;
